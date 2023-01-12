ff68bf9cbce694a8db6d3f0791d33863c0543336
Speed431 <73215393+Speed431@users.noreply.github.com>
2023-01-12 16:48:47 +0700

    added babel js

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,16 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     },
