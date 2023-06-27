7ec790ed8c5dffdb28a5ff325dbf6062dae33d85
shodon <asrorovribis@gmail.com>
2023-06-27 09:36:09 +0700

    updated styles in mobile

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 
 import appList from './appList';
 import Nav from './components/nav/Nav';
@@ -8,10 +9,11 @@ import styles from './Projects.module.scss';
 
 
 const Projects = ({ active }) => {
+    let lang = useSelector(state => state.lang);
     let [category, setCategory] = useState('landing');
     return (
         <div className={styles.main}>
-            <div className={styles.title}>Projects</div>
+            <div className={styles.title}>{lang === 'ru' ? 'Проекты' : 'Projects'}</div>
             <Nav category={category} setCategory={setCategory} />
             <List category={category} appList={appList} />
         </div>
--- src/components/projects/components/nav/Nav.jsx
+++ src/components/projects/components/nav/Nav.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 import styles from './Nav.module.scss';
+import { useSelector } from 'react-redux';
 
 const Nav = ({ category, setCategory }) => {
+    let lang = useSelector(state => state.lang);
     return (
         <div className={styles.main}>
             <input
                 type='button'
-                value={'landing'}
+                value={lang === 'ru' ? 'верстки' : 'landing'}
                 onClick={() => setCategory('landing')}
                 className={(category == 'landing' ? styles.buttonActive : null) + ' ' + (styles.button)}
             />
             <input
                 type='button'
-                value={'react'}
+                value={lang === 'ru' ? 'реакт' : 'react'}
                 onClick={() => setCategory('react')}
                 className={(category == 'react' ? styles.buttonActive : null) + ' ' + (styles.button)}
             />
